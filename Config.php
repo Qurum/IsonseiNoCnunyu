3015d33b8edac861391659ef3cbf0a8307ef4008
Ronja <qurum@yandex.ru>
2021-10-24 02:35:33 +0300

    Add config decorator for YAML

---
+++ Config.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * Copyright (c) Ronia Rebane 2021.
+ * Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted.
+ */
+
+namespace Abethropalle\IsonseiNoChunyu;
+
+use Symfony\Component\Yaml\Yaml;
+
+class Config implements \ArrayAccess
+{
+    protected $container = [];
+
+    public function __construct($path){
+        $this->container = Yaml::parseFile($path);
+    }
+
+    public function offsetSet($offset, $value) {
+        if (is_null($offset)) {
+            $this->container[] = $value;
+        } else {
+            $this->container[$offset] = $value;
+        }
+    }
+
+    public function offsetExists($offset) {
+        return isset($this->container[$offset]);
+    }
+
+    public function offsetUnset($offset) {
+        unset($this->container[$offset]);
+    }
+
+    public function offsetGet($offset) {
+        return isset($this->container[$offset]) ? $this->container[$offset] : null;
+    }
+}
