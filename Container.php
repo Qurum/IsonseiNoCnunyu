409283f1107e6ceab560130712f521c3fabbb7c7
Ronja <qurum@yandex.ru>
2021-10-26 01:33:50 +0300

    Add has and get methods to Container

--- Container.php
+++ Container.php
@@ -6,14 +6,73 @@
 
 namespace Abethropalle\IsonseiNoChunyu;
 
+use Abethropalle\Utils\Graph\DepthFirstSearchCycleDetector;
 use Abethropalle\Utils\Graph\GraphBuilder;
 
 class Container
 {
-    protected ServiceAssemblyProvider$service_provider;
+    protected ServiceAssemblyProvider $service_provider;
+    protected Assembler $assembler;
+    protected array $instances = [];
 
     public function setServiceAssemblyProvider(ServiceAssemblyProvider $sp)
     {
         $this->service_provider = $sp;
+    }
+
+    public function setAssembler(Assembler $asm){
+        $this->assembler = $asm;
+    }
+
+    protected function getDependenciesTuples($service_name){
+        $service = $this->service_provider->get($service_name);
+        $args = $service->getConstructorArgs();
+        if(empty($args)){
+            return [];
+        } else{
+            $result = [];
+            foreach ($args as $arg){
+                if(str_starts_with($arg, 'str:')){ continue;}
+                $result[] = [$service_name, $arg];
+                $result = array_merge($result, $this->getDependenciesTuples($arg));
+            }
+            return $result;
+        }
+    }
+
+    protected function checkCircularDependencies($name)
+    {
+        $graph = new GraphBuilder();
+        $callback = fn($arrow) => $graph->arrow($arrow[0], $arrow[1]);
+        $dependencies = $this->getDependenciesTuples($name);
+        array_walk($dependencies,$callback);
+        $d = new DepthFirstSearchCycleDetector($graph->build());
+        $result = ['result' => $d->detect()];
+        if($result['result']){$result['cycle'] = $d->getCycle();}
+        return (object)$result;
+    }
+
+    public function has($name){
+        return $this->service_provider->has($name);
+    }
+
+    public function get($name){
+        if(is_null($this->service_provider)){
+            throw new \Exception('Провайдер сервисов не установлен');
+        }
+
+        if(!isset($this->instances[$name])){
+            $assembly = $this->service_provider->get($name);
+            $has_circular = $this->checkCircularDependencies($name);
+            if($has_circular->result){
+                throw new \Exception("Циклическая зависимость:" . $has_circular->cycle);
+            } else {
+                if(is_null($this->assembler)){
+                    throw new \Exception('Ассемблер не установлен');
+                }
+                $this->instances[$name] = $this->assembler->assemble($assembly);
+            }
+        }
+        return $this->instances[$name];
     }
 }
