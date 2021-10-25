c9c64ed77c80307a1fffa9187d7de9f999556457
Ronja <qurum@yandex.ru>
2021-10-25 18:03:04 +0300

    Add ClassDependenciesInspectorTest.php

---
+++ tests/src/ClassDependenciesInspectorTest.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * Copyright (c) Ronia Rebane 2021.
+ * Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted.
+ */
+
+use Abethropalle\IsonseiNoChunyu\ClassDependenciesInspector;
+use PHPUnit\Framework\TestCase;
+
+class ClassDependenciesInspectorTest extends TestCase
+{
+    /**
+     * @dataProvider providerConstructorDependencies
+     */
+    public function testGetConstructorDependencies(string $class_name, array $dependencies)
+    {
+        $cdi = new ClassDependenciesInspector($class_name);
+        $this->assertEquals($dependencies, $cdi->getConstructorDependencies());
+    }
+
+    public function providerConstructorDependencies()
+    {
+        return [
+            'EchoLogger' =>
+                [
+                    '\Abethropalle\DataTransferProjectStub\EchoLogger',
+                    []
+            ],
+
+            'FileLogger' =>
+                [
+                    '\Abethropalle\DataTransferProjectStub\FileLogger',
+                    [
+                        '\Abethropalle\DataTransferProjectStub\FileManagerInterface',
+                        'string'
+                    ]
+                ],
+
+            'Main' =>
+                [
+                    '\Abethropalle\DataTransferProjectStub\Main',
+                    [
+                        '\Abethropalle\DataTransferProjectStub\ProviderInterface',
+                        '\Abethropalle\DataTransferProjectStub\ResolverInterface',
+                        '\Abethropalle\DataTransferProjectStub\LoggerInterface'
+                    ]
+                ],
+
+            'Resolver' =>
+                [
+                    '\Abethropalle\DataTransferProjectStub\Resolver',
+                    [
+                        '\Abethropalle\DataTransferProjectStub\AdapterInterface',
+                        '\Abethropalle\DataTransferProjectStub\AdapterInterface',
+                        '\Abethropalle\DataTransferProjectStub\AdapterInterface'
+                    ]
+                ]
+        ];
+    }
+}
